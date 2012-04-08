c74841b805e6f6d45fdca0d03dd1db4e568a298b
MiksLatvis <MiksLatvis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-08 08:17:51 +0000

    added first tagged version
    git-svn-id: https://plugins.svn.wordpress.org/post-series/tags/0.4.1@528768 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,56 @@
+=== Post Series ===
+Contributors: mikslatvis
+Donate link: http://mikslatvis.com/wordpress-plugins/post-series/
+Tags: post series, series
+Requires at least: 3.0
+Tested up to: 3.3.1
+Stable tag: trunk
+
+Add one or multiple posts to a post series, and Post Series will automatically show simple post list by all posts in the series.
+ 
+== Description ==
+Post series plugin adds functionality to make series of posts. It also displays a list of those posts at the beginning of all posts in this series.
+
+= Features =
+
+* Add post to a one or multiple post series.
+* Display list of the posts in the series at the beginning of the post.
+* Extra functionality will follow.
+
+== Installation ==
+
+1. Download the plugin
+2. Copy the post-series folder to the "/wp-content/plugins/" directory 
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. You are done – you can now go into a post and place it into a post series.
+
+Post Installation
+1. Go and edit the post you wand to include in a post series
+2. Add the name of the post series you want to use.
+3. Update post and have a look at you new post series.
+4. Go to another post and give the same series name.
+
+== Frequently Asked Questions ==
+
+= How do I show the list of posts? =
+
+List of posts appears automatically by all the posts included in Post Series.
+
+== Screenshots == 
+
+1. This is a new edit box, which shows up in a right sidebar by your post.
+2. This is how list of posts looks like after some posts are published.
+
+== Change log ==
+
+= 0.4.1 - 31/03/2012 =
+* Minor changes to plugin Read me
+* Added plugin screenshots
+
+= 0.4 - 30/03/2012 =
+* Plugin published to WordPress plugin directory
+
+= 0.3 - 16/03/2012 =
+* Plugin ready with minimum features
+
+== Upgrade Notice ==
---
+++ series-of-posts.php
@@ -0,0 +1,92 @@
+<?php
+/*
+Plugin Name: Post Series
+Plugin URI: http://mikslatvis.com/wordpress-plugins/post-series/
+Description: Allows you to add posts to a series and show the list by the posts.
+Version: 0.4.1
+Author: Miks Latvis
+Author URI: http://mikslatvis.com
+License: GPL2
+*/
+
+
+wp_enqueue_style( 'my-style', plugins_url( '/style.css', __FILE__ ), false, '1.0', 'all' ); // Inside a plugin
+
+function series_of_posts_init() {
+	// create a new taxonomy
+	register_taxonomy(
+		'series_of_posts',
+		'post',
+		array(
+			'label' => __( 'Series of posts' ),
+			'sort' => true,
+			'args' => array( 'orderby' => 'term_order' ),
+			'rewrite' => array( 'slug' => 'series_of_posts' )
+		)
+	);
+}
+add_action( 'init', 'series_of_posts_init' );
+
+
+add_filter( 'the_content', 'series_of_posts_filter', 20 );
+/**
+ * Add a series box to the beginning of post page wich have a series.
+ *
+ */
+function series_of_posts_filter( $content ) {
+
+    if ( is_single() && has_term('', 'series_of_posts') )
+        
+        {
+
+$terms = get_terms('series_of_posts');
+ $count = count($terms);
+ if ( $count > 0 ){
+     echo "<div id='series-of-posts-box'><ul>";
+     foreach ( $terms as $term ) {
+       echo "<p class='one-series'>" . $term->name . ":</p>";
+       
+       $series_of_posts = $term->slug; 
+		
+		$args = array(
+	'tax_query' => array(
+		array(
+			'taxonomy' => 'series_of_posts',
+			'field' => 'slug',
+			'terms' => $series_of_posts
+		)
+	)
+);
+
+       
+      $the_query = new WP_Query( $args );
+
+// The Loop
+while ( $the_query->have_posts() ) : $the_query->the_post();
+	echo '<li class="one-link">'; ?>
+	<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+
+	
+	<?php echo '</li>';
+endwhile;
+
+// Reset Post Data
+wp_reset_postdata(); 
+
+
+ ?>
+       
+       
+     <?php   
+     }
+     echo "</ul></div>";
+ }
+
+
+    // Returns the content.
+    return $content; } else {return $content;}
+}
+
+
+
+?>
